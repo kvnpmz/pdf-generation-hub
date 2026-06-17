57beaeff2200da3d9290c36e4d710a6fc0a2f59d
kvnpmz <kvnpmz@protonmail.com>
2026-06-17 18:10:31 -0400

    Refactored lua/c# to strategy pattern

--- TemplatePrintable/RenderStrategy.cs
+++ TemplatePrintable/RenderStrategy.cs
@@ -7,42 +7,24 @@ public class RenderStrategy : IPipelineStep
         using var lua = new Lua();
         lua.DoString("require('init')");
 
-        string docPath = $"documents/{context.DocId}";
-        string configContent = await File.ReadAllTextAsync($"{docPath}/config.tl"); 
+        var setupFunction = (LuaFunction)lua["SetupDocument"];
+        var results = (object[])setupFunction.Call(context.DocId);
 
-        var luaResults = lua.DoString(configContent);
-        if (luaResults.Length == 0)
-            throw new Exception("config.tl returned no value");
+        var config = (LuaTable)results[0];
+        var renderTemplate = results[1];
 
-        var config = (LuaTable)luaResults[0];
         string outputName = config["output_name"]?.ToString() ?? config["id"]?.ToString() ?? "output";
         string template = config["template"]?.ToString() ?? "default";
 
-        string renderScript = await File.ReadAllTextAsync($"templates/{template}/render.tl");
-        var renderTemplate = lua.DoString(renderScript)[0];
-
-        var data = new {
-            config = config,
-            renderTemplate = renderTemplate
-        };
-
-        lua["data"] = data;
+        lua["data"] = new { config = config, renderTemplate = renderTemplate };
         var generateDoc = (LuaFunction)lua["GenerateDocument"];
 
-        var result = generateDoc.Call(lua["data"]);
-        string html = (string)result[0];
+        string html = (string)generateDoc.Call(lua["data"])[0];
+        var applyStyling = (LuaFunction)lua["ApplyStyling"];
 
-        string baseCss = await File.ReadAllTextAsync("templates/base/style.css");
-        string cssPath = $"templates/{template}/style.css";
-        string customCss = File.Exists(cssPath)
-            ? await File.ReadAllTextAsync(cssPath)
-            : "";
+        html = (string)applyStyling.Call(html, config["template"])[0];
+        Console.WriteLine(html);
 
-        string styleBlock = $"<style>{baseCss}</style><style>{customCss}</style></head>";
-        html = html.Replace("</head>", styleBlock);
-
-//        Console.WriteLine(html);
-//        return (html, outputName);
         context.Html = html;
         context.OutputName = outputName;
     }
--- TemplatePrintable/init.lua
+++ TemplatePrintable/init.lua
@@ -3,3 +3,8 @@
 local tl = require('tl')
 tl.loader()
 require("templates.base.render")
+local builder = require("builder")
+local style_provider = require("style_provider")
+
+SetupDocument = builder.Compose
+ApplyStyling = style_provider.Apply
