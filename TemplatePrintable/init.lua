c7b2f2c8d9e4d2c367fb8e3d28be233d61de52c6
kvnpmz <kvnpmz@protonmail.com>
2026-06-17 20:05:34 -0400

    Added more strategy-based structure to lua

--- TemplatePrintable/init.lua
+++ TemplatePrintable/init.lua
@@ -2,8 +2,8 @@ local tl = require('tl')
 tl.loader()
 
 require("templates.base.render")
-local builder = require("builder")
+local context = require("context")
 local style_provider = require("style_provider")
 
-SetupDocument = builder.Compose
+SetupDocument = context.Execute
 ApplyStyling = style_provider.Apply
