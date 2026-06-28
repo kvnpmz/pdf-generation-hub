f281e9042b7a58d365a6bac77300f28a6e30f1f1
kvnpmz <kvnpmz@protonmail.com>
2026-06-28 05:58:26 -0400

    Renamed context data in generator.tl

--- TemplatePrintable/init.lua
+++ TemplatePrintable/init.lua
@@ -1,28 +1,5 @@
-local M = {}
+local generator = require("generator")
 
-local render = require("templates.base.render")
-local context = require("context")
-local style_provider = require("style_provider")
-
-
-
-
-
-
-function M.Render(document_id)
-   local config, render_template = context.Execute(document_id)
-
-   local html = render.GenerateDocument({
-      config = config,
-      renderTemplate = render_template,
-   })
-
-   html = style_provider.Apply(html, config)
-
-   return {
-      html = html,
-      output_name = config.output_name or config.id or "output",
-   }
-end
-
-return M
+return {
+   Render = generator.Render,
+}
