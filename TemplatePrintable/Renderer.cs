2c0cf13565972aa0c2f55f0ee0cbff88fed520bb
kvnpmz <kvnpmz@protonmail.com>
2026-06-28 05:16:15 -0400

    save before moving lua logic back to lua

--- TemplatePrintable/Renderer.cs
+++ TemplatePrintable/Renderer.cs
@@ -11,23 +11,25 @@ public class Renderer : IPipelineStep
         using var lua = new Lua();
         lua.State.Encoding = System.Text.Encoding.UTF8; 
 
-        lua.DoString("require('init')");
-        var setupFunction = (LuaFunction)lua["SetupDocument"];
+        lua.DoString("local tl = require('tl'); tl.loader();");
+        var exports = (LuaTable)lua.DoString("return require('init')")[0];
 
+        var setupFunction = (LuaFunction)exports["SetupDocument"];
         var results = (object[])setupFunction.Call(context.DocumentId);
+
         var config = (LuaTable)results[0];
         var renderTemplate = (LuaFunction)results[1];
 
         string outputName = config["output_name"]?.ToString() ?? config["id"]?.ToString() ?? "output";
         string template = config["template"]?.ToString() ?? "default";
 
-        lua["data"] = new { config = config, renderTemplate = renderTemplate };
-        var generateDocument = (LuaFunction)lua["GenerateBase"];
+        lua["data"] = new { config, renderTemplate };
+        var generateDocument = (LuaFunction)exports["GenerateBase"];
 
         string html = (string)generateDocument.Call(lua["data"])[0];
-        var applyStyling = (LuaFunction)lua["ApplyStyling"];
+        var styling = (LuaFunction)exports["ApplyStyling"];
 
-        html = (string)applyStyling.Call(html, config)[0];
+        html = (string)styling.Call(html, config)[0];
         File.WriteAllText("preview.html", html);
 
         context.Html = html;
--- TemplatePrintable/init.lua
+++ TemplatePrintable/init.lua
@@ -1,10 +1,9 @@
-local tl = require('tl')
-tl.loader()
-
 local render = require("templates.base.render")
 local context = require("context")
 local style_provider = require("style_provider")
 
-GenerateBase = render.GenerateDocument
-ApplyStyling = style_provider.Apply
-SetupDocument = context.Execute
+return {
+   GenerateBase = render.GenerateDocument,
+   ApplyStyling = style_provider.Apply,
+   SetupDocument = context.Execute,
+}
