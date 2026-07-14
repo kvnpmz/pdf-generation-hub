ef689d01c864d6341077d9d42bf1dae69739e179
kvnpmz <kvnpmz@protonmail.com>
2026-07-14 01:26:27 -0400

    Added dorm list

---
+++ templates/dorm_list/render.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using NLua;
+
+namespace DormList;
+
+public class TemplateRender : IRenderer
+{
+    public RenderResult Render(LuaTable config)
+    {
+        var headerHtml = Dom.H1("", config["header"]?.ToString() ?? "");
+
+        var columnsHtml = new List<string>();
+
+        var columns = (LuaTable)config["columns"];
+
+        foreach (LuaTable columnItems in columns.Values)
+        {
+            var columnContent = new List<string>();
+
+            foreach (LuaTable section in columnItems.Values)
+            {
+                var itemsHtml = new List<string>();
+
+                foreach (var item in ((LuaTable)section["items"]).Values)
+                {
+                    itemsHtml.Add(Dom.Li("", item?.ToString() ?? ""));
+                }
+
+                columnContent.Add(
+                        Dom.Section("",
+                            Dom.H2("", section["title"]?.ToString() ?? ""),
+                            Dom.Ul("", itemsHtml.ToArray())
+                            )
+                        );
+            }
+
+            columnsHtml.Add(
+                    Dom.Div("column", columnContent.ToArray())
+                    );
+        }
+
+        columnsHtml.Insert(0, headerHtml);
+
+        return new RenderResult
+        {
+            Html = string.Concat(columnsHtml),
+            OutputName = config["output_name"]?.ToString() ?? config["id"]?.ToString() ?? "output"
+        };
+    }
+}
